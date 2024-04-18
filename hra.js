e1b0598257848b25fbaa90be8b74e75b12b5179c
Tatiana Kohoutova <tatiana.belozerova1543@gmail.com>
2024-04-18 23:02:04 +0200

    ukol 4.

--- hra.js
+++ hra.js
@@ -1,11 +1,11 @@
-console.log('funguji!');
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
 let currentPlayer = 'circle';
+const pole = document.querySelectorAll('.pole');
 
 const chod = (evt) => {
   const btn = evt.target;
   evt.preventDefault();
-  // btn.classList.add('board__field--circle');
   evt.target.disabled = true;
 
   if (currentPlayer === 'circle') {
@@ -16,49 +16,58 @@ const chod = (evt) => {
     btn.classList.add('board__field--cross');
   }
 
+  const hraje = document.querySelector('.kolecko');
   if (currentPlayer === 'circle') {
-    const hraje = document.querySelector('.kolecko');
     hraje.innerHTML = `<img src="images/circle.svg" alt="Hraje kolecko"/>`;
   } else {
-    const hraje = document.querySelector('.kolecko');
     hraje.innerHTML = `<img src="images/cross.svg" alt="Hraje krizek"/>`;
   }
+
+  let herniPole = [];
+  let vysledek = null;
+  pole.forEach((hodnota) => {
+    if (hodnota.classList.contains('board__field--circle')) {
+      vysledek = 'o';
+    } else if (hodnota.classList.contains('board__field--cross')) {
+      vysledek = 'x';
+    } else {
+      vysledek = '_';
+    }
+    herniPole.push(vysledek);
+  });
+
+  const vyhral = findWinner(herniPole);
+  const KoleckoIsBest = () => {
+    alert('Kolečko je borec!');
+    location.reload();
+  };
+  const KrizekIsBest = () => {
+    alert('X dobře ty! Kolečko se může jít klouzat');
+    location.reload();
+  };
+  const noWinners = () => {
+    alert('Smůla, nemáme vítěze, ani poraženého. Zkuste to znova');
+    location.reload();
+  };
+
+  if (vyhral === 'o') {
+    setTimeout(KoleckoIsBest, 160);
+  } else if (vyhral === 'x') {
+    setTimeout(KrizekIsBest, 160);
+  } else if (vyhral === 'tie') {
+    setTimeout(noWinners, 160);
+  }
+
+  // console.log(herniPole, findWinner(herniPole));
 };
 
-const pole = document.querySelectorAll('.pole');
 pole.forEach((button) => {
   button.addEventListener('click', chod);
 });
 
-// Může se stát, že uživatel se omylem uklikne a modrým tlačítkem pro restart přijde o rozehranou hru. Proto přidej modrému odkazu posluchač události, který se po kliknutí uživatele zeptá zabudovanou funcí confirm, jestli chce hru opravdu restartovat. Pokud ne, zavolej event.preventDefault(), čímž zabráníš tomu, aby prohlížeč přešel na odkazovanou stránku (tj. načetl stránku s prázdnou hrou). Funkce confirm vrací true nebo false podle toho, zda uživatel souhlasil nebo nesouhlasil s potvrzovací zprávou v dialogu.
-
-// const confirm = () => {
-//   // const znovu = e.target;
-//   confirm('Opravdu chceš začít znovu?');
-// };
-
 document.querySelector('.button-znovu').addEventListener('click', (e) => {
-  // confirm('Opravdu chceš začít znovu?');
   if (confirm('Opravdu chceš začít znovu?') == false) {
     e.preventDefault();
   } else {
   }
 });
-
-// if (confirm == true) {
-//   let otazka = (e) => {
-//     e.preventDefault();
-//   };
-// }
-
-// form.addEventListener('submit', (e) => {
-//   if (fname.value === '' || lname.value === '') {
-//     e.preventDefault();
-//     para.textContent = 'You need to fill in both names!';
-//   }
-// });
-// let otazka = (e) => {
-//   if (confirm('Opravdu chceš začít znovu?') == true) {
-//     e.target.preventDefault();
-//   }
-// };
